3385765af8c60d5d65fb53372893094cf7bb78fa
Zhao Song Zhou <zhaosongzhouu@gmail.com>
2025-05-21 19:04:06 -0400

    Set up Vite for Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://ZhaoSongZh7.github.io/AI-Lesson-Generator',
   plugins: [react(),
      tailwindcss(),
   ],
